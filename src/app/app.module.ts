25004a4c7e3eb0603ad0f555d0bb1b14589e508a
Tolik Dem <tolik.demianov@gmail.com>
2021-02-24 18:20:43 +0300

    пример связки переменной с инпутом. С помощью [(ngModel)] директивы

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
+/*
+FormsModule позволяет использовать директиву [(ngModel)] в шаблоне, ну и видимо еще что-то связанное с формами.
+*/
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule, /* Её нужно помещать сюда, чтобы ангуляр её смог заюзать во свех компонентах относящихся к этому модулю.
+    Так как app.module.ts является корневым компонентом, то дериктивы из FormsModule будут доступны во всём приложении.
+    Надеюсь я правильно понял концепцию NgModules.
+    */
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
