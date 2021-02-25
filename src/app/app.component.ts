ab286d84a85edb0200fed7711cee3b4e1c738b12
Tolik Dem <tolik.demianov@gmail.com>
2021-02-25 13:21:02 +0300

    поигрался со стилями

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  // styleUrls: ['./app.component.scss'] // здесь массив, потому-что можно несколько файлов стилей подключать, если надо.
+  styles: [`
+    h3 {
+      color: indianred;
+    }
+  `]
 })
 export class AppComponent {
   name = 'Tolik';
