376bf6405ba3202b512c33aec43cbe7fbafca2ef
Tolik Dem <tolik.demianov@gmail.com>
2021-02-27 20:45:45 +0300

    сделал базовую разметку в компоненте recipe-detail
    и сделал выпадающий список dropdown тамже

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-detail.component.scss']
 })
 export class RecipeDetailComponent implements OnInit {
+  isDropdownVisible = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onClickDropdown(): void {
+    this.isDropdownVisible = !this.isDropdownVisible;
+  }
 }
