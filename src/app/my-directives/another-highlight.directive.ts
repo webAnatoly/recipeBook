d1bb27b7f153d48d436abf4747a987174c9c2484
Tolik Dem <tolik.demianov@gmail.com>
2021-03-01 21:56:47 +0300

    пример директивы с property binging

--- src/app/my-directives/another-highlight.directive.ts
+++ src/app/my-directives/another-highlight.directive.ts
@@ -1,23 +1,37 @@
-import { Directive, HostBinding, HostListener } from '@angular/core';
+import { Directive, HostBinding, HostListener, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appAnotherHighlight]'
 })
-export class AnotherHighlightDirective {
-  @HostBinding('style.background') background = 'lightyellow';
-  @HostBinding('style.padding') padding = '3px 5px';
-  @HostBinding('style.borderRadius') borderRadius = '4px';
-  @HostBinding('style.border') border = '2px solid transparent';
+export class AnotherHighlightDirective implements OnInit {
+  @Input() bg = 'lightyellow';
+  @Input() bgHover = 'red';
+
+  @HostBinding('style.background') background!: string;
+  @HostBinding('style.padding') padding!: string;
+  @HostBinding('style.borderRadius') borderRadius!: string;
+  @HostBinding('style.border') border!: string;
 
   @HostListener('mouseenter') mouseover(): void {
     this.border = '2px solid black';
-    this.background = 'red';
+    this.background = this.bgHover;
   }
   @HostListener('mouseleave') mouseleave(): void {
     this.border = '2px solid transparent';
-    this.background = 'lightyellow';
+    this.background = this.bg;
+  }
+
+  ngOnInit(): void {
+  this.background = this.bg;
+  this.padding = '3px 5px';
+  this.borderRadius = '4px';
+  this.border = '2px solid transparent';
   }
 
   constructor() { }
 
 }
+
+/*
+custom event binding also works in directives.
+ */
