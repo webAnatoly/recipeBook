127c048c67eea1bc944c30379d1e41fc75b1a540
Tolik Dem <tolik.demianov@gmail.com>
2021-03-01 20:30:13 +0300

    пример добавление к кастомной дерективе события с помощью @HostListener
    точнее к элементу, к которому будет применяться директива.
    
    В этом коммите к дериктиве better-highlight добавили появление тени при наведении мышки.

--- src/app/my-directives/better-highlight.directive.ts
+++ src/app/my-directives/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
@@ -20,6 +20,19 @@ export class BetterHighlightDirective implements OnInit{
     this.renderer.setStyle(this.elRef.nativeElement, 'padding', '5px');
     this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'black');
     this.renderer.setStyle(this.elRef.nativeElement, 'border-radius', '4px');
+    this.renderer.setStyle(this.elRef.nativeElement, 'transition', 'box-shadow .3s ease-out');
   }
 
+  @HostListener('mouseenter') mouseover(eventData: Event): void {
+    this.renderer.setStyle(this.elRef.nativeElement, 'box-shadow', `2px 2px 5px 1px rgba(0,0,0,0.5),
+    -2px -2px 5px 1px rgba(0,0,0,0.5)`);
+  }
+  @HostListener('mouseleave') mouseleave(eventData: Event): void {
+    this.renderer.setStyle(this.elRef.nativeElement, 'box-shadow', 'none');
+  }
+
+  /*
+  К слову сказать @HostListener может реагировать не только на встроенные DOM события, но и на кастомные,
+  которые можно самому создавать с помощью EventEmitter.
+   */
 }
