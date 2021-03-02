43d97a61ea9c28a74bfe48833e79c65b802d5d79
Tolik Dem <tolik.demianov@gmail.com>
2021-03-02 16:07:28 +0300

    еще коммент про @Injectable добавил

--- src/app/accounts.service.ts
+++ src/app/accounts.service.ts
@@ -7,6 +7,16 @@ import { Injectable } from '@angular/core';
   @Injectable tells Angular that now this service is injectable or that something can be injected in there to be precise.
   You don't add @Injectable to the service you want to inject but to the service where you want to inject something.
 
+  if you don't want to inject anything in a service you don't need to add @Injectable.
+  Only add it if you expect to get something injected.
+
+  @Injectable - помечает данный класс, как класс который будет подвергаться инжектирования, т.е. в этот класс
+  будет инжектироваться зависимость, например другой сервис.
+
+  Вот строка из документации: @Injectable - Decorator that marks a class as available to be provided and injected as a dependency.
+
+  Если я правильно понял из курса, то ангуляр рекомендует всем сервисам добавлять декоратор @Injectable даже если
+  не планируется в данный сервис что-то инжектить. Ну на всякий случай, вдруг в будущем запланируется.
 */
 @Injectable()
 export class AccountsService {
