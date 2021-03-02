3908fb68c1535593fb2131afcff24ee10268db86
Tolik Dem <tolik.demianov@gmail.com>
2021-03-02 16:01:37 +0300

    @Injectable - добавил в сервис в который инжектирую другой сервис и таким образом устранил ошибку, которая была коммитом ранее

--- src/app/accounts.service.ts
+++ src/app/accounts.service.ts
@@ -1,5 +1,14 @@
 import { LoggingService } from './logging.service';
+import { Injectable } from '@angular/core';
 
+/* if you inject a service into something, this something needs to have some metadata attached to it.
+   Такие метаданные даем ангуляру с помощью директивы @Injectable
+
+  @Injectable tells Angular that now this service is injectable or that something can be injected in there to be precise.
+  You don't add @Injectable to the service you want to inject but to the service where you want to inject something.
+
+*/
+@Injectable()
 export class AccountsService {
   accounts = [
     {
@@ -20,9 +29,11 @@ export class AccountsService {
 
   addAccount(name: string, status: string): void {
     this.accounts.push({name, status});
+    this.loggingService.logStatusChange(status);
   }
 
   updateStatus(id: number, status: string): void {
     this.accounts[id].status = status;
+    this.loggingService.logStatusChange(status);
   }
 }
