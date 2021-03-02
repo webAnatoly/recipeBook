7b6df0fa76b83a521eaa65601b0f8ba6d989614c
Tolik Dem <tolik.demianov@gmail.com>
2021-03-02 13:37:15 +0300

    проинжектировал LoginService в account.component

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,18 +1,21 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { LoggingService } from '../logging.service';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.scss']
+  styleUrls: ['./account.component.scss'],
+  providers: [LoggingService], // informs Angular how to create this loggingService
 })
 export class AccountComponent {
   @Input() account: {name: string, status: string} = {name: 'default name', status: 'default status'};
   @Input() id = 0;
   @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
+  constructor(private loggingService: LoggingService) {}
 
   onSetTo(status: string): void {
     this.statusChanged.emit({id: this.id, newStatus: status});
-    console.log('A server status changed, new status: ' + status);
+    this.loggingService.logStatusChange(status);
   }
 }
