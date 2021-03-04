449b4a9e91d7331f45c3fae43ba03bc929a09d27
Tolik Dem <tolik.demianov@gmail.com>
2021-03-04 11:22:11 +0300

    показ сервера по клику на сервере

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -10,10 +11,13 @@ import { ServersService } from '../servers.service';
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string} = {id: 0, name: '', status: ''};
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.server = this.serversService.getServer(1);
+    this.route.params.subscribe((params) => {
+        this.server = this.serversService.getServer(+params.id);
+    });
   }
 
 }
