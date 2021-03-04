ec74fe8b587cf9e204fb468f08d4781d4ae3de09
Tolik Dem <tolik.demianov@gmail.com>
2021-03-04 10:48:01 +0300

    Пример получения ?query параметров внутри компонента

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit-server',
@@ -12,12 +13,31 @@ export class EditServerComponent implements OnInit {
   serverName = '';
   serverStatus = '';
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+              private route: ActivatedRoute) { }
+
+/*  private route: ActivatedRoute - инжектируем роутеровский сервис ActivatedRoute, чтобы с его помощью получать
+  ?query параметры и #fragment из url*/
 
   ngOnInit(): void {
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
+
+    /* Первый способ получения параметров это получение их из snapshot
+    * Помним, что snapshot создается только один раз при создании компонента и при обновлении компонента
+    * snapshot больше не обновляется */
+    console.log('route.snapshot.queryParams', this.route.snapshot.queryParams);
+    console.log('route.snapshot.fragment', this.route.snapshot.fragment);
+
+    /* Второй способ это подписаться на Observable, который возвращается из this.route.queryParams
+    * В этом случае при update компонента обсервер будет эмитить значения, и если они изменились мы получим новые значения */
+    this.route.queryParams.subscribe((queryParam) => {
+      console.log('queryParam', queryParam);
+    });
+    this.route.fragment.subscribe((fragment) => {
+      console.log('fragment', fragment);
+    });
   }
 
   onUpdateServer(): void {
