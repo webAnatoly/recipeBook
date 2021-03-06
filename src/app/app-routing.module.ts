c0343574d927420e093b3cbd6d6340e53ef87e30
Tolik Dem <tolik.demianov@gmail.com>
2021-03-06 16:45:22 +0300

    added activeRoute directives to links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipesComponent } from './recipes/recipes.component';
 
 const routes: Routes = [
-  { path: '', component: RecipesComponent },
+  { path: '', redirectTo: '/recipes', pathMatch: 'full', },
   { path: 'recipes', component: RecipesComponent },
   { path: 'shopping-list', component: ShoppingListComponent },
 ];
@@ -14,10 +13,9 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    RouterModule.forRoot(routes),
-  ],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule] // Now since we are in an extra module here and we want to get this into our main module,
+  // into our AppModule, we need to export this readily configured router.
+
 })
 export class AppRoutingModule { }
