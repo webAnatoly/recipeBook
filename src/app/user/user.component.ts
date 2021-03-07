c8f63e6f104c9d8ede5c7613d40ae7c59aa59814
Tolik Dem <tolik.demianov@gmail.com>
2021-03-07 11:38:44 +0300

    Добавил коммент разбирающий пример с observable

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -15,6 +15,29 @@ export class UserComponent implements OnInit {
     this.route.params.subscribe((params) => {
       this.id = +params.id;
     });
+    /*
+    params here (this.route.params) is an observable to which we subscribe.
+
+    Observables are constructs to which you subscribe to be informed about the changes in data
+    because remember, observables are that stream of data and whenever a new data piece is emitted,
+    our subscription will know about it.
+
+    So in this case here, params is the observable, it's a stream of route parameters and that stream
+    gives us a new route parameter whenever we go to a new page, whenever that parameter in the URL changes
+    and then in callback function we pass to .subscribe(), we get the new params and we can extract our relevant param,
+    in this case the ID param.
+    */
+
+    /*
+    So that how this built-in observable works and how you can think about it,
+    params is the observable, it's that stream of data that gives us new values.
+    */
+
+    /*
+    Now that's all nice and Angular will heavily use such observables
+    and there you will never need to create them yourselves, you only subscribe to them, you don't need to create them
+    but to understand them, it certainly doesn't hurt to also create them.
+    */
   }
 
 }
