cf23be5b812fa5c8535dc52288d9bc3897a5e2f5
Tolik Dem <tolik.demianov@gmail.com>
2021-03-07 14:46:32 +0300

    Создал свой собсвенный Observable с помощью устаревшего статического метода .create() класса Observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { interval, Subscription } from 'rxjs';
+import { interval, Observable, Subscription } from 'rxjs';
+import { MyCustomObservableService } from '../shared/my-custom-observable.service';
 // interval() Creates an Observable that emits sequential numbers every specified interval of time
 /*
 Помним, что по Ctrl+Click на функции можно перейти в её определение и там в комментах документация такая же как и на сайте
@@ -15,6 +16,7 @@ https://rxjs.dev/api/index/function/interval
 export class HomeComponent implements OnInit, OnDestroy {
 
   private firstObsSubscription!: Subscription;
+  private secondObsSubscription!: Subscription;
   /* What is a Subscription?
   A Subscription is an object that represents a disposable resource, usually the execution of an Observable.
   A Subscription has one important method, unsubscribe, that takes no argument
@@ -23,7 +25,8 @@ export class HomeComponent implements OnInit, OnDestroy {
   https://rxjs.dev/guide/subscription
    */
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+              private myCustomObservable: MyCustomObservableService) { }
 
   ngOnInit(): void {
     // Будем создавать свой Observable
@@ -38,11 +41,15 @@ export class HomeComponent implements OnInit, OnDestroy {
 
     // Метод .subscribe() возвращает тип Subscription, который нужен, чтобы отписываться от observable
     // а отписываться иногда нужно, чтобы не было утечки памяти
-/*
-   Отписываться от встроенных ангуляровских observables не нужно потому что ангуляр сам от них отписывается.
-   For the observables provided by Angular, like params and others, all these Angular observables
-   are managed by Angular and therefore, you don't need to unsubscribe manually here, Angular will take care about this.
-*/
+    /*
+    Отписываться от встроенных ангуляровских observables не нужно потому что ангуляр сам от них отписывается.
+    For the observables provided by Angular, like params and others, all these Angular observables
+    are managed by Angular and therefore, you don't need to unsubscribe manually here, Angular will take care about this.
+    */
+
+    this.secondObsSubscription = this.myCustomObservable.myCustomIntervalObservable.subscribe((value: number) => {
+      console.log('my second observable!', value);
+    });
   }
 
   ngOnDestroy(): void {
@@ -53,5 +60,6 @@ export class HomeComponent implements OnInit, OnDestroy {
     при каждом уходе с компонента.
     Поэтому нужно отписываться.
      */
+    this.secondObsSubscription.unsubscribe();
   }
 }
---
+++ src/app/shared/my-custom-observable.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MyCustomObservableService {
+
+  myCustomIntervalObservable!: Observable<number>;
+
+  constructor() {
+    // В качестве тренировки создаем свой observable вызовом статического метода .create() класса Observable
+    // метод create() вроде как деприкейтед, но ради тренировки попробуем и так и так посоздавать.
+    this.myCustomIntervalObservable = Observable.create((observer: any) => {
+      /* Observer - The observer, in the end, is a part that is interested in being informed about new data,
+      about errors, or about the observable being completed.
+
+      Now our job here is to tell the observer about new data, about an error or about the observable being completed.
+
+      Here, we're not responsible for listening because the observer is the listener,
+      here we get that listening part as an argument and we need to tell it once we're done,
+      once new data is there and so on.
+
+      So in this anonymous function, we can now use the regular set setInterval method.
+
+      */
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        count += 1;
+        // We can call next here to emit a new value.
+        // The observer has a couple of important methods next(), error(), complete()
+      }, 1000);
+    });
+  }
+}
