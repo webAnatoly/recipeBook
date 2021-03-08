2b67fc7dc0ab2dceda0b280e1f6df207fcc177ae
Tolik Dem <tolik.demianov@gmail.com>
2021-03-08 19:58:52 +0300

    Пример получения объекта формы через @ViewChild

--- src/app/view-child-example/view-child-example.component.ts
+++ src/app/view-child-example/view-child-example.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-view-child-example',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-child-example.component.scss']
 })
 export class ViewChildExampleComponent implements OnInit {
+  // Пример получения объекта формы через @ViewChild
+  @ViewChild('f') signUpForm!: NgForm;
+  /*
+  Такой способ полезен потому что можно получать доступ к объкту формы не только во время Submit, но и раньше, если надо.
+   */
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    console.log(this.signUpForm);
   }
 
 }
