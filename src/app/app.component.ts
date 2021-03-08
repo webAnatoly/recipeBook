2b1ec2db84c51b6418f0143ada2e793aec632768
Tolik Dem <tolik.demianov@gmail.com>
2021-03-08 17:49:59 +0300

    Example of getting access to the form object (NgForm) created by Angular based on form in template.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -13,5 +14,12 @@ export class AppComponent implements OnInit {
 
   suggestUserName(): void {
     const suggestedName = 'Superuser';
+  }
+
+  onSubmit(form: NgForm): void {
+    // form это объект типа NgForm, представляющий форму и её поля.
+    // Этот объект создается ангуляром на основе верстки формы в шаблоне.
+    // И этот объект мы передали сюда из шаблона вот таким образом <form (ngSubmit)="onSubmit(currentForm)" #currentForm="ngForm">
+    console.log('Submitted!', form);
   }
 }
