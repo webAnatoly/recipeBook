a0a56c75db79902d3b76b63c385337a86afde0e9
Tolik Dem <tolik.demianov@gmail.com>
2021-03-11 19:13:40 +0300

    added unsubscribe()

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss']
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, OnDestroy {
   recipes: Recipe[] = [];
+  subsription!: Subscription;
 
   constructor(private recipeService: RecipeService,
               private router: Router,
@@ -17,9 +19,13 @@ export class RecipeListComponent implements OnInit {
 
   ngOnInit(): void {
     this.recipes = this.recipeService.getRecipes();
-    this.recipeService.recipeChanged.subscribe((recipes: Recipe[]) => {
+    this.subsription = this.recipeService.recipeChanged.subscribe((recipes: Recipe[]) => {
       this.recipes = recipes;
     });
+  }
+
+  ngOnDestroy(): void {
+    this.subsription.unsubscribe();
   }
 
   onNewRecipe(): void {
