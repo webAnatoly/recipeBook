98786f75a2e195a59f1783cfd775b575550be8b4
Tolik Dem <tolik.demianov@gmail.com>
2021-03-12 12:03:21 +0300

    пример с параметром по-умолчанию

--- src/app/shorten.pipe.ts
+++ src/app/shorten.pipe.ts
@@ -4,9 +4,9 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'shorten'
 })
 export class ShortenPipe implements PipeTransform{
-  transform(value: string): string {
-    if ( value.length > 15 ) {
-      return value.substr(0, 15) + ' ...';
+  transform(value: string, limit: number = 15): string {
+    if ( value.length > limit ) {
+      return value.substr(0, limit) + ' ...';
     }
     return value;
   }
