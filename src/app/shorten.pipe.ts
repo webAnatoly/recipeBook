052de3b5919be47ca51e5e2483f0037c2c8a762e
Tolik Dem <tolik.demianov@gmail.com>
2021-03-12 12:00:11 +0300

    ShortenPipe improved

--- src/app/shorten.pipe.ts
+++ src/app/shorten.pipe.ts
@@ -4,10 +4,10 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'shorten'
 })
 export class ShortenPipe implements PipeTransform{
-  // transform(value: any, ...args): any {
-  //   return value;
-  // }
-  transform(value: any): any {
-    return value.substr(0, 10);
+  transform(value: string): string {
+    if ( value.length > 15 ) {
+      return value.substr(0, 15) + ' ...';
+    }
+    return value;
   }
 }
