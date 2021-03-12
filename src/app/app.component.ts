f82975a0fa752235828e82ece7db77841025ced6
Tolik Dem <tolik.demianov@gmail.com>
2021-03-12 15:25:58 +0300

    http request example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,8 +14,19 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {}
 
   onCreatePost(postData: { title: string; content: string }): void {
-    // Send Http request
-    console.log(postData);
+    const endpoint = 'https://ng-project-881e4-default-rtdb.firebaseio.com/';
+    /*
+    API может быть разным. У каждого API свой API :)
+    Читаем документацию или спрашиваем у бекенд разработчика куда и в каком виде слать запросы.
+    В этом мини-приложении используем в качестве бекенда Firebase.
+    */
+
+    this.http.post(
+      `${endpoint}posts.json`,
+      postData
+    ).subscribe((responseData) => {
+      console.log('responseData', responseData);
+    });
   }
 
   onFetchPosts(): void {
