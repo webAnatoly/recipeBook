28cc7e71cb0b340559dd40c6fdb852778da234fa
Tolik Dem <tolik.demianov@gmail.com>
2021-03-23 14:58:17 +0300

    добавил обновление рецептов после получения их с серверва

--- src/app/recipes/recipe-start/recipe-start.component.ts
+++ src/app/recipes/recipe-start/recipe-start.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RecipeService } from '../recipe.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-recipe-start',
   templateUrl: './recipe-start.component.html',
   styleUrls: ['./recipe-start.component.scss']
 })
-export class RecipeStartComponent implements OnInit {
+export class RecipeStartComponent implements OnInit, OnDestroy {
   isRecipesExists = false;
+  subscriptionRecipeChanged!: Subscription;
 
   constructor(private recipeService: RecipeService,
               private router: Router,
@@ -16,10 +18,17 @@ export class RecipeStartComponent implements OnInit {
 
   ngOnInit(): void {
     this.isRecipesExists = Boolean(this.recipeService.amount);
+    this.subscriptionRecipeChanged = this.recipeService.recipeChanged.subscribe((recipe) => {
+      this.isRecipesExists = Boolean(this.recipeService.amount);
+    });
   }
 
   onNewRecipe(): void {
     this.router.navigate(['new'], {relativeTo: this.route})
       .catch(error => console.error(error));
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptionRecipeChanged.unsubscribe();
   }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -10,24 +10,26 @@ import { Subject } from 'rxjs';
 export class RecipeService {
   recipeChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'A Test Recipe',
-      'Description',
-      'https://cdn.kqed.org/wp-content/uploads/sites/24/2013/09/PumpkinHoneyBread.jpg',
-      [
-        new Ingredient('Apple', 2),
-        new Ingredient('Flour', 3),
-      ]),
-    new Recipe(
-      'A Test Recipe2',
-      'Description2',
-      'https://cdn.stocksnap.io/img-thumbs/960w/food-recipe_G8QICMKLUV.jpg',
-      [
-        new Ingredient('Bread', 1),
-        new Ingredient('Butter', 1),
-      ]),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'A Test Recipe',
+  //     'Description',
+  //     'https://cdn.kqed.org/wp-content/uploads/sites/24/2013/09/PumpkinHoneyBread.jpg',
+  //     [
+  //       new Ingredient('Apple', 2),
+  //       new Ingredient('Flour', 3),
+  //     ]),
+  //   new Recipe(
+  //     'A Test Recipe2',
+  //     'Description2',
+  //     'https://cdn.stocksnap.io/img-thumbs/960w/food-recipe_G8QICMKLUV.jpg',
+  //     [
+  //       new Ingredient('Bread', 1),
+  //       new Ingredient('Butter', 1),
+  //     ]),
+  // ];
+
+  private recipes: Recipe[] = [];
 
   get amount(): number { // возвращает кол-во рецептов
     return this.recipes.length;
